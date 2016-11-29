7c553b78194c13b44564c2650fb0f2fb2c97e647
Sergii Kryshtop <ksa@3si2.com>
2016-11-30 00:20:54 +0200

    adding the code of sample application

---
+++ CityInfo.API/src/CityInfo.API/Startup.cs
@@ -0,0 +1,81 @@
+﻿using AutoMapper;
+using CityInfo.API.Models;
+using CityInfo.DAL.Entities;
+using CityInfo.DAL.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+
+namespace CityInfo.API
+{
+	public class Startup
+	{
+		public static IConfigurationRoot Configuration;
+
+		public Startup(IHostingEnvironment environment)
+		{
+			var builder = new ConfigurationBuilder()
+			.SetBasePath(environment.ContentRootPath)
+			.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+			.AddJsonFile($"appsettings.{environment.EnvironmentName}.json", optional: false, reloadOnChange: true);
+			Configuration = builder.Build();
+		}
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc()
+					.AddMvcOptions(o => o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter()));
+
+			var connectionString = Configuration["ConnectionStrings:DBConnection"];
+			services.AddScoped<IDataStorage, DataStorage>(_ => new DataStorage(connectionString));
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole();
+			loggerFactory.AddDebug();
+			loggerFactory.AddNLog();
+
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				app.UseExceptionHandler();
+			}
+
+			app.UseMvc();
+
+			app.Run(async (context) =>
+			{
+				await context.Response.WriteAsync("Hello World!");
+			});
+
+			ConfigureAutomapper();
+		}
+
+		private static void ConfigureAutomapper()
+		{
+			Mapper.Initialize(cfg =>
+			{
+				cfg.CreateMap<City, CityDto>();
+				cfg.CreateMap<City, CityWithoutPointsOfInterestDto>();
+				cfg.CreateMap<CityForCreationDto, City>();
+				cfg.CreateMap<CityForUpdateDto, City>();
+				cfg.CreateMap<PointOfInterest, PointOfInterestDto>();
+				cfg.CreateMap<PointOfInterestForCreationDto, PointOfInterest>();
+				cfg.CreateMap<PointOfInterestForUpdateDto, PointOfInterest>();
+			});
+		}
+	}
+}
---
+++ CityInfo.API/src/CityInfo.DAL/Repositories/DataStorage.cs
@@ -0,0 +1,53 @@
+﻿using CityInfo.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.DAL.Repositories
+{
+	public class DataStorage : IDataStorage
+	{
+		private CityInfoContext _context;
+
+		private IRepository<City> _cities;
+		private IRepository<PointOfInterest> _pointsOfInterest;
+
+		public DataStorage(string connectionString)
+		{
+			var builder = new DbContextOptionsBuilder<CityInfoContext>();
+			builder.UseSqlServer(connectionString);
+
+			_context = new CityInfoContext(builder.Options);
+			_context.EnsureSeedDataForContext();
+		}
+
+		public IRepository<City> Cities
+		{
+			get
+			{
+				if (_cities == null)
+				{
+					_cities = new CitiesRepository(_context);
+				}
+
+				return _cities;
+			} 
+		}
+
+		public IRepository<PointOfInterest> PointsOfInterest
+		{
+			get
+			{
+				if (_pointsOfInterest == null)
+				{
+					_pointsOfInterest = new PointsOfInterestRepository(_context);
+				}
+
+				return _pointsOfInterest;
+			}
+		}
+
+		public void SaveChanges()
+		{
+			_context.SaveChanges();
+		}
+	}
+}
