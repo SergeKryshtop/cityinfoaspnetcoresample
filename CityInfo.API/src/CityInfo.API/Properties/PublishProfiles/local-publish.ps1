7365bc16b4f57d330bbd4b4bef8fada68fe5724b
Sergii Kryshtop <ksa@3si2.com>
2016-12-05 02:01:31 +0200

    added Local publish profile

---
+++ CityInfo.API/src/CityInfo.API/Properties/PublishProfiles/local-publish.ps1
@@ -0,0 +1,19 @@
+﻿[cmdletbinding(SupportsShouldProcess=$true)]
+param($publishProperties=@{}, $packOutput, $pubProfilePath)
+
+# to learn more about this file visit https://go.microsoft.com/fwlink/?LinkId=524327
+
+try{
+    if ($publishProperties['ProjectGuid'] -eq $null){
+        $publishProperties['ProjectGuid'] = 'c77d43e1-8f63-48ec-a757-16b14f2b90b6'
+    }
+
+    $publishModulePath = Join-Path (Split-Path $MyInvocation.MyCommand.Path) 'publish-module.psm1'
+    Import-Module $publishModulePath -DisableNameChecking -Force
+
+    # call Publish-AspNet to perform the publish operation
+    Publish-AspNet -publishProperties $publishProperties -packOutput $packOutput -pubProfilePath $pubProfilePath
+}
+catch{
+    "An error occurred during publish.`n{0}" -f $_.Exception.Message | Write-Error
+}
