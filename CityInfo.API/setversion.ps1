38522c50dd94aa7b9e367691772c0c56140994a4
Sergii Kryshtop <skryshtop@colemanrg.com>
2017-01-17 22:59:14 +0200

    added script to replace the netcore version

--- CityInfo.API/setversion.ps1
+++ CityInfo.API/setversion.ps1
@@ -1,3 +1,11 @@
-﻿$a = Get-Content .\project.json -raw | ConvertFrom-Json
-$a.version = "0.2.*"
-$a | ConvertTo-Json  | set-content .\project.json
+﻿[CmdletBinding()]
+Param(
+	[Parameter(Mandatory=$true, Position=1)]
+	$version,
+    [Parameter(Mandatory=$false, Position=2)]
+	$filePath = ".\src\CityInfo.API\project.json"
+)
+
+$a = Get-Content $filePath -raw | ConvertFrom-Json
+$a.version = $version
+$a | ConvertTo-Json  | set-content $filePath
