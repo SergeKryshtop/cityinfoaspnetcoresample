21870b1ed9cab87f345f86a0b27865ebb37797f9
Sergii Kryshtop <skryshtop@colemanrg.com>
2017-01-18 20:55:03 +0200

    add logging to see the connection string

--- CityInfo.API/src/CityInfo.DAL/Repositories/DataStorage.cs
+++ CityInfo.API/src/CityInfo.DAL/Repositories/DataStorage.cs
@@ -1,18 +1,24 @@
 ﻿using CityInfo.DAL.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace CityInfo.DAL.Repositories
 {
 	public class DataStorage : IDataStorage
 	{
 		private CityInfoContext _context;
+	    private ILogger _logger;
 
-		private IRepository<City> _cities;
+        private IRepository<City> _cities;
 		private IRepository<PointOfInterest> _pointsOfInterest;
 
 		public DataStorage(string connectionString)
 		{
-			var builder = new DbContextOptionsBuilder<CityInfoContext>();
+            _logger = new LoggerFactory().CreateLogger<DataStorage>();
+            var builder = new DbContextOptionsBuilder<CityInfoContext>();
+
+            _logger.LogInformation("Connection string: " + connectionString);
+            
 			builder.UseSqlServer(connectionString);
 
 			_context = new CityInfoContext(builder.Options);
